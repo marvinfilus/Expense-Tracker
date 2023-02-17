547e0496ddb4df5d4e91ed04b79b42d2e4494393
Fresh Prince <freshprince@Freshs-Air.lan>
2023-02-17 10:14:33 -0500

    Finished

--- expense/src/context/AppReducer.js
+++ expense/src/context/AppReducer.js
@@ -1,6 +1,14 @@
 export default (state , action ) => {
     switch(action.type){
+        case "Add_Transaction":
+            return {...state,
+                transactions: [...state.transactions, action.info]
+            }
+            case "Delete_Transaction":
+                return {...state,
+                    transactions: state.transactions.filter(trans => trans.id !== action.info)
+                }
         default:
-             return state;
+        return state;        
     }
 }
--- expense/src/context/GlobalState.js
+++ expense/src/context/GlobalState.js
@@ -10,14 +10,29 @@ const initialState = {
     ]
 }
 
+
 //create context
 export const GlobalContext = createContext(initialState)
 
 // Provider component
 export const GlobalProvider = ({ children }) => {
 const [ state, dispatch] = useReducer(AppReducer, initialState);
+const deleteTransaction = (id) => {
+    dispatch({
+        type: 'Delete_Transaction',
+        info : id
+    })
+}
+const addTransaction = (transaction) => {
+    dispatch({
+        type: 'Add_Transaction',
+        info : transaction
+    })
+}
 return ( <GlobalContext.Provider value={{
-    transactions:state.transactions
+    transactions:state.transactions,
+    deleteTransaction,
+    addTransaction
 }}>
     {children}
 </GlobalContext.Provider>)
